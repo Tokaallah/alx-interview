7077a6c5b7f6840e27696779182827d2c7ae4104
Tokallah Hussein <54765267+Tokaallah@users.noreply.github.com>
2024-04-23 14:36:36 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -26,3 +26,59 @@ def isWinner(x, nums):
     if marias_wins == bens_wins:
         return None
     return 'Maria' if marias_wins > bens_wins else 'Ben'
+
+
+
+
+
+
+
+
+
+
+{
+    "transaction_timestamp": "2024-02-15T12:25:08+00:00", //yyyy-MM-ddTHH:mm:ssz
+    "shipped_from_site_code": "6299999999981",
+    "shipped_to_site_code": "6299999999998",
+    "biz_transactions": { // for available keyss, see section 7.3.3 in https://www.gs1.org/sites/default/files/docs/epc/CBV-Standard-1-2-2-r-2017-10-12.pdf
+        "desadv": "6299999999981", // despatch advice | delivery number
+        "po": "0081030898", // purchase order
+        "inv": "656846513" // invoice
+    },
+    "items": [
+        {
+            // unit item information (GTIN, batch, exp, mfg, serial)
+            "barcode": "136R49H6GHGE3A",
+            "GTIN": "03664798022643",
+            "batch_number": "X3C131V",
+            "expiry": "2025-09-30",
+            "manufacturing_date": "2023-03-20",
+            // aggregation hierarchy (up to 7 parnets, typically 3 in most cases)
+            "parent1": "136600530023657654",
+            "parent2": "236600530023657654",
+            "parent3": "336600530023657654",
+            "parent4": "436600530023657654"
+        },
+        {
+            "GTIN": "03664798022643",
+            "batch_number": "X3C131V",
+            "expiry": "2025-09-30",
+            "manufacturing_date": "2023-03-20",
+            "barcode": "136R49H6GHHK27",
+            "parent1": "936600530023657654",
+            "parent2": "836600530066857069"
+        }
+        // add all the items in same manner
+    ],
+    "external_values": {
+        // Any other value
+    }
+}
+
+
+
+
+
+
+
+
